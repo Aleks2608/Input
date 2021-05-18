822422a41b5790e252d92376af4459cef4ac3b72
Aleks Wall <84318292+Aleks2608@users.noreply.github.com>
2021-05-18 13:31:00 +0200

    Das ist mein Testversuch

---
+++ Input/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Input
+//
+//  Created by Aleks der könig der welt Wall on 18.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
